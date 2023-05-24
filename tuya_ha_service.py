6f2481a594e4283f62726cfa433a3f286cede564
Gaurav Kuwar <gauravskuwar@gmail.com>
2023-05-24 19:43:41 -0400

    Fixed bug where if one tuya device was unreachable, the whole system was unusable

--- tuya_ha_service.py
+++ tuya_ha_service.py
@@ -8,6 +8,7 @@ import signal
 import random
 import json
 import tinytuya
+import threading
 
 from pyhap.accessory import Accessory, Bridge
 from pyhap.accessory_driver import AccessoryDriver
@@ -27,25 +28,28 @@ class LightBulb(Accessory):
         super().__init__(*args, **kwargs)
         
         self.i = dev_idx
-        
-        # initialize tuya device
+        logging.info(f"Connecting to {devices[self.i]['name']} ...")
         self.tuya_dev = tinytuya.BulbDevice(
             devices[self.i]['id'], 
             devices[self.i]['ip_addr'], 
             local_key=devices[self.i]['local_key'], 
             dev_type='default')
         
+        self.tuya_dev.set_socketRetryLimit(1)
+        self.tuya_dev.set_socketTimeout(0.5)
         self.tuya_dev.set_version(devices[self.i]['version'])
         
-        self.ha_state = self.tuya_state() # get initial state from tuya
-        
+        self.ha_state = 0 # get initial state from tuya
+           
         # initialize homekit device
         serv_light = self.add_preload_service('Lightbulb')
         self.char_on = serv_light.configure_char(
             'On', value=self.ha_state, setter_callback=self.set_bulb)
+        
+        print(devices[self.i]['name'], self.ha_state)
     
     def set_bulb(self, value):
-        if self.tuya_state() != 2:
+        if self.tuya_state() != "Error":
             if value: 
                 self.tuya_dev.turn_on()
             else:
@@ -53,22 +57,30 @@ class LightBulb(Accessory):
 
             self.ha_state = self.tuya_state()
             logging.info(f"{devices[self.i]['name']} value: {value}")
+        else:
+            self.char_on.set_value(0) 
+            
     
     # only in the case lights are changed from tuya app, or other sources.
     @Accessory.run_at_interval(5)
     async def run(self):
-        if self.ha_state != self.tuya_state():
-            self.char_on.set_value(self.tuya_state())
-
-    def tuya_state(self):
+        cur_tuya_state = self.tuya_state()
+        
+        if cur_tuya_state == "Error":
+            self.char_on.set_value(0) 
+            
+        elif self.ha_state != cur_tuya_state:
+            self.char_on.set_value(cur_tuya_state) 
+        
+    def tuya_state(self):        
         tuya_status = self.tuya_dev.status()
         if 'dps' in tuya_status:
             on_off_num = '1' if devices[self.i]['version'] == 3.1 else "20"
             return tuya_status['dps'][on_off_num]
         else:
             # we can't have the tuya app open at the same time as this service
-            logging.info("Error: close other tuya connections!")
-            return 2 # error
+            logging.info(f"Error in {devices[self.i]['name']}: {tuya_status}")
+            return "Error" # error
 
 def get_bridge(driver):
     bridge = Bridge(driver, 'Tuya Hub')
